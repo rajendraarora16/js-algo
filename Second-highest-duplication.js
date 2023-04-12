d4ab973806d3e1ed0c2e2da48a46a83e0b7b144a
rajendraarora16 <contact.rajendraarora@gmail.com>
2023-04-12 16:57:46 +0530

    added algo

---
+++ Second-highest-duplication.js
@@ -0,0 +1,20 @@
+/**
+ * Given an array, find the most duplicate occuring value.
+ */
+
+let arr = ["a", "b", "c", "b", "a", "a"]
+let hashMap = {};
+
+arr.forEach(value => {
+  
+  if (value in hashMap) {
+    hashMap[value]++;  
+  } else {
+    hashMap[value] = 0;
+  }
+  return hashMap;
+});
+
+let keyValRes = Object.values(hashMap);
+let result = keyValRes.sort(function(a, b) { return b - a })[1];
+console.log(result);
