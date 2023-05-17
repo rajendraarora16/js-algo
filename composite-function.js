659f7de6710ffc65f093c28d90c5da6af709b1b4
Rajendra arora <contact.rajendraarora@gmail.com>
2023-05-17 14:49:50 +0530

    Update composite-function.js

--- composite-function.js
+++ composite-function.js
@@ -1,7 +1,10 @@
-const pipe = (...fns) => x => fns.reduce((acc, currentItr) => currentItr(acc), x); 
+const compositeFunc = (...args) => x => args.reduce((acc, currentItr) => currentItr(acc), x);
 const double = x => x * 2
 const square = x => x * x
 
 // function pipe
-var output_final = pipe(square, double)(2);
+var output_final = compositeFunc(square, double)(5);
 console.log(output_final);
+
+// Output
+// 50
