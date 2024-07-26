56c40adc879969884984d08bde4a4d9855d26ab0
Rajendra arora <contact.rajendraarora@gmail.com>
2024-07-26 16:28:04 +0530

    added fixes for locale compare

--- Sort-ascending-descending.js
+++ Sort-ascending-descending.js
@@ -1,4 +1,6 @@
 
+// Numbers --> "(a - b) or (b - a)"
+
 let arr = [1, 2, 3, 3, 4, 5, 6, 7, 7, 8, 8, 22, 90, 224];
 
 // Ascending
@@ -6,3 +8,18 @@ arr.sort((a, b) => (a-b));
 
 // Descending
 arr.sort((a, b) => (b-a));
+
+
+// Characters --> "a.localeCompare(b) or b.localeCompare(a)"
+
+let charArr = ["a", "e", "c", "f", "b", "d", "g", "i", "a"];
+
+// Ascending..
+charArr.sort((a, b) => a.localeCompare(b))
+
+// Descending..
+charArr.sort((a, b) => b.localeCompare(a));
+
+console.log(charArr)
+
+
