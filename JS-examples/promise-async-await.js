917f77e9c89a73fdb23ebce705129404b8beb5b4
Rajendra arora <contact.rajendraarora@gmail.com>
2024-08-11 09:58:41 +0530

    promise learning

--- JS-examples/promise-async-await.js
+++ JS-examples/promise-async-await.js
@@ -31,3 +31,42 @@ async function getAsyncCall() {
 }
 
 getAsyncCall();
+
+
+// -----------------------------------------------------------------------
+// Types of Promise
+//------------------------------------------------------------------------
+
+const promise1 = new Promise((resolve, _reject) => setTimeout(() => resolve("promise 1 is resolved"), 500));
+const promise2 = new Promise((_resolve, reject) => setTimeout(() => reject("promise 2 is rejected"), 800));
+const promise3 = new Promise((resolve, _reject) => setTimeout(() => resolve("promise 3 is resolved"), 1000));
+
+
+// returns whoever first is resolved
+Promise.any([promise1, promise2, promise3])
+    .then((value) => console.log("value:", value))
+    .catch((error) => console.log("error: ",error));
+
+
+// returns whoever first is resolved or rejected
+Promise.race([promise1, promise2, promise3])
+    .then((value) => console.log("value:", value))
+    .catch((error) => console.log("error: ",error));
+
+
+// returns once all the promise is resolved and rejected
+Promise.allSettled([promise1, promise2, promise3])
+    .then((value) => console.log("value:", value))
+    .catch((error) => console.log("error: ",error));
+
+
+// returns once all the promise is resolved if there is a rejection, 
+// it stops immediately and returns a rejection
+Promise.all([promise1, promise2, promise3])
+    .then((value) => console.log("value:", value))
+    .catch((error) => console.log("error: ",error));
+
+
+
+
+
