76fea8c25e0cad5811584444ce8924b39fa3285f
Rajendra arora <contact.rajendraarora@gmail.com>
2025-03-03 10:10:28 +0530

    Update two-pointer-algo.js

--- two-pointer-algo.js
+++ two-pointer-algo.js
@@ -22,3 +22,41 @@ let target = -2;
 
 console.log(twoSum(arr, target));
 
+
+/**
+ * 
+ * Time Complexity: O(n) as the loops runs at most n times. We either increase left, or decrease right or stop the loop.
+ * Auxiliary Space: O(1)
+ * 
+ */
+const twoSum = (arr, target) => {
+
+  arr.sort((a, b) => a - b);
+
+  let left = 0; 
+  let right = arr.length - 1;
+
+  while (left < right) {
+
+    let sum = arr[left] + arr[right];
+    
+    if (sum === target) {
+      return true;
+    } 
+
+    else if (sum < target) {
+      left++;
+    } 
+    else {
+      right--;
+    }
+  }
+
+  return false;
+}
+
+let arr = [0, -1, 2, -3, 1];
+let target = -2
+
+console.log(twoSum(arr, target));
+
