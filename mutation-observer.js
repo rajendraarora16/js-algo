b7ea82958fef33bcab0f216e7c06e2ba3b0cea01
Rajendra arora <contact.rajendraarora@gmail.com>
2026-03-07 21:04:08 +0530

    Update mutation-observer.js

--- mutation-observer.js
+++ mutation-observer.js
@@ -1,10 +1,37 @@
-const observer = new MutationObserver((mutations) => {
-  mutations.forEach(mutation => {
-    console.log("DOM is changed: ", mutation);
-  });
+/**
+ * Intersection observer
+ * 
+ * It is used to detect the visibility of element
+ * It is used to check whether element is in viewport or not.
+ * It can be used to trigger animation when element is in viewport
+ * Ad view tracking.
+ */
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if(entry.isIntersecting) {
+      console.log("element is visible..");
+    }
+  })
+});
+observer.observe(document.querySelector("#box"));
+
+
+/**
+ * Mutation observer
+ * 
+ * It is used to track element changes
+ * It is used to track text changes
+ * Element is added or removed so that be tracked
+ * Detect DOM update changes
+ */
+const observer = new MutationObserver((entries) => {
+  entries.forEach(entry => {
+    console.log("DOM changed: ", entry);
+  })
 });
 
 observer.observe(document.body, {
   childList: true,
   subtree: true,
-});
+})
