eac2ba93ac4f8ca5fac2d8c5329dfbc28404f3de
Rajendra arora <contact.rajendraarora@gmail.com>
2026-04-05 13:38:58 +0530

    Create BST-simple.js

---
+++ BST-simple.js
@@ -0,0 +1,51 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BST {
+
+  constructor () {
+    this.root = null;
+  }
+
+  add (value) {
+    if (!this.root) {
+      this.root = new Node(value);
+    } else {
+      this.insert(this.root, value);
+    }
+  }
+
+  insert (node, value) {
+
+    // left leaf..
+    if (node.value > value) {
+      if (!node.left) {
+        node.left = new Node(value);
+      } 
+      else {
+        this.insert(node.left, value);
+      }
+    }
+    else {
+      if (!node.right) {
+        node.right = new Node(value);
+      }
+      else {
+        this.insert(node.right, value);
+      }
+    }
+  }
+}
+
+const bst = new BST();
+bst.add(30);
+bst.add(3);
+bst.add(40);
+bst.add(39);
+bst.add(83);
+bst.add(300);
