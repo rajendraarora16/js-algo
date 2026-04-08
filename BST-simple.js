b55240cac06401b8ac4fce3d6c3c76ac70adc13b
Rajendra arora <contact.rajendraarora@gmail.com>
2026-04-08 10:32:38 +0530

    Update BST-simple.js

--- BST-simple.js
+++ BST-simple.js
@@ -1,5 +1,5 @@
 class Node {
-  constructor(value) {
+  constructor (value) {
     this.value = value;
     this.left = null;
     this.right = null;
@@ -7,32 +7,36 @@ class Node {
 }
 
 class BST {
-
   constructor () {
     this.root = null;
   }
 
   add (value) {
+
     if (!this.root) {
       this.root = new Node(value);
-    } else {
-      this.insert(this.root, value);
+    }
+    else {
+      this.insert (this.root, value);
     }
   }
 
   insert (node, value) {
 
-    // left leaf..
+    // assign to left leaf..
     if (node.value > value) {
+
       if (!node.left) {
         node.left = new Node(value);
-      } 
+      }
       else {
         this.insert(node.left, value);
       }
     }
-    // right leaf..
+
+    // assign to right leaf..
     else {
+
       if (!node.right) {
         node.right = new Node(value);
       }
@@ -41,12 +45,35 @@ class BST {
       }
     }
   }
+
+  search (target) {
+
+    let node = this.root;
+
+    while (node != null) {
+
+      if (node.value == target) {
+        return true;
+      }
+
+      if (node.value > target) {
+        node = node.left;
+      }
+      else {
+        node = node.right;
+      }
+    }
+
+    return false;
+  }
 }
 
 const bst = new BST();
-bst.add(30);
-bst.add(3);
+
 bst.add(40);
-bst.add(39);
-bst.add(83);
-bst.add(300);
+bst.add(20);
+bst.add(10);
+bst.add(6);
+bst.add(80);
+
+console.log(bst.search(80))
