5cfc7a1ecde4f679255b69aad0076e00e1cd25f1
Rajendra arora <contact.rajendraarora@gmail.com>
2026-04-11 22:46:44 +0530

    Create variable-sliding-window-with-k.js

---
+++ variable-sliding-window-with-k.js
@@ -0,0 +1,46 @@
+const variableSlidingWindow = (arr, k) => {
+  
+  let left = 0;
+  let right = 0;
+  
+  let maxCount = 0;
+  let hashMap = {};
+  let typesCount = 0;
+
+  while (right < arr.length) {
+
+    if (typesCount < k || arr[right] in hashMap) {
+
+      if (!hashMap[arr[right]]) {
+        typesCount++;
+        hashMap[arr[right]] = 1;
+      }
+      else {
+        hashMap[arr[right]]++;
+      }
+
+      maxCount = Math.max(maxCount, right - left + 1);
+
+      right++;
+    }
+
+    else {
+
+      hashMap[arr[left]]--;
+
+      if (hashMap[arr[left]] === 0) {
+
+        typesCount--;
+        delete hashMap[arr[left]];
+      }
+      left++;
+    }
+  }
+
+  return maxCount;
+}
+
+const str = "araaci";
+const k = 2
+console.log(variableSlidingWindow(str, k));
+// 4
