522c068984e1826cc23cc9be3512e56296da9f11
Rajendra arora <contact.rajendraarora@gmail.com>
2026-04-12 12:28:16 +0530

    Update variable-sliding-window-with-k.js

--- variable-sliding-window-with-k.js
+++ variable-sliding-window-with-k.js
@@ -44,3 +44,37 @@ const str = "araaci";
 const k = 2
 console.log(variableSlidingWindow(str, k));
 // 4
+
+
+
+// find longest sub array with sum <= k
+
+
+const variableSlidingWindow = (arr, k) => {
+
+  let left = 0;
+  let right = 0;
+  let maxCount = 0;
+  let sum = 0;
+
+  while (right < arr.length) {
+
+    sum += arr[right];
+
+    while (sum >= k) {
+      sum -= arr[left];
+      left++;
+    }
+
+    maxCount = Math.max(maxCount, right - left + 1);
+
+    right++;
+  }
+
+  return maxCount;
+}
+
+const arr = [1, 2, 1, 0, 1, 1, 0];
+const k = 4;
+
+console.log(variableSlidingWindow(arr, k));
